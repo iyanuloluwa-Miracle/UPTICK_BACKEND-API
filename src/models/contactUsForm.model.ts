d050c9cb987ac8b5fada127e6cea71cbe6313238
iyanuloluwa-Miracle <iyanudina@gmail.com>
2023-11-12 07:14:14 +0100

    feat:contact U

---
+++ src/models/contactUsForm.model.ts
@@ -0,0 +1,43 @@
+import { DataTypes, Model } from 'sequelize';
+import sequelize  from '../config/database';
+
+class ContactUs extends Model {
+    public id!: number;
+    public name!: string;
+    public email!: string;
+    public phone!: string;
+    public description!: string;
+  }
+  
+  ContactUs.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      phone: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'ContactUs',
+    }
+  );
+  
+  export default ContactUs;
+  
