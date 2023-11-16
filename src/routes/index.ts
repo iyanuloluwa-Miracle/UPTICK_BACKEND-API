901938f674260859526e84c0cd78b5ecc0632bef
iyanuloluwa-Miracle <iyanudina@gmail.com>
2023-11-16 15:54:32 +0100

    fix:Bug fix- remove a force command in the server.ts file

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import mailRoute from "./mail.route";
 import applicantRoute from "./applicant.route";
 import programRoute from "./program.route";
 import jobRoute from "./job.route";
+import contactRoute from "./contactUsRoutes"
 
 const router: Router = express.Router();
 
@@ -27,6 +28,10 @@ const defaultRoutes: DefaultRoute[] = [
   {
     path: "/jobs",
     route: jobRoute,
+  },
+  {
+    path: "/contact",
+    route: contactRoute,
   },
 ];
 
