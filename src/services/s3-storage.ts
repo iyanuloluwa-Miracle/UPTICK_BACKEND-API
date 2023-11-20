2a558b2e11c4597253447fa38425a1f8f10793b6
Joshua Farayola <farayolajoshua@gmail.com>
2023-11-20 14:26:25 +0100

    feat: implement s3 storage service

---
+++ src/services/s3-storage.ts
@@ -0,0 +1,75 @@
+import {
+  DeleteObjectCommand,
+  GetObjectCommand,
+  PutObjectCommand,
+  S3Client,
+} from "@aws-sdk/client-s3";
+import { randomBytes } from "crypto";
+import StorageService, { StoragePutOptions } from "./interfaces/storage";
+
+export default class S3StorageService implements StorageService {
+  private s3Client: S3Client;
+
+  constructor(private readonly bucketName: string) {
+    this.s3Client = new S3Client({ region: "us-east-2" });
+  }
+
+  public async put(
+    buffer: Buffer,
+    opts?: StoragePutOptions | undefined
+  ): Promise<string> {
+    const randomString = randomBytes(8).toString("hex");
+    const key = opts?.filename
+      ? `${Date.now()}-${opts.filename}`
+      : `${Date.now()}-${randomString}.png`;
+
+    try {
+      const command = new PutObjectCommand({
+        Bucket: this.bucketName,
+        Key: key,
+        Body: buffer,
+      });
+      await this.s3Client.send(command);
+    } catch (error) {
+      console.error(error);
+      throw new Error("An error occurred while uploading a file");
+    }
+
+    return `https://${this.bucketName}.s3.amazonaws.com/${key}`;
+  }
+
+  public async get(url: string): Promise<Buffer> {
+    const key = this.getKey(url);
+    try {
+      const command = new GetObjectCommand({
+        Bucket: this.bucketName,
+        Key: key,
+      });
+      const res = await this.s3Client.send(command);
+      const body = await res.Body?.transformToByteArray();
+
+      return body ? Buffer.from(body) : Buffer.from("");
+    } catch (error) {
+      console.error(error);
+      throw new Error("An error occurred while retrieving a file");
+    }
+  }
+
+  public async delete(url: string): Promise<void> {
+    const key = this.getKey(url);
+    try {
+      const command = new DeleteObjectCommand({
+        Bucket: this.bucketName,
+        Key: key,
+      });
+      await this.s3Client.send(command);
+    } catch (error) {
+      console.error(error);
+      throw new Error("An error occurred while deleting a file");
+    }
+  }
+
+  private getKey(url: string) {
+    return url.split("amazonaws.com/")[1];
+  }
+}
